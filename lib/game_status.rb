2d92cff7e617bcf9d6d8c5e4c1de23d6b7dd4572
Trevor Seitz <trevinla@gmail.com>
2018-03-05 16:53:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,7 +47,7 @@ end
 def winner(board)
   WIN_COMBINATIONS.find do |win_combo|
     if board[win_combo[0]] == board[win_combo[1]] && board[win_combo[0]] == board[win_combo[2]] && position_taken?(board, win_combo[1])
-      board[win_combo[0]] 
+      board[win_combo[1]] 
     end
   end
 end
